b56aa7ca59afbe99f62785cb79bf8c4541ab70e5
Serena Ludueña <sereluduena@gmail.com>
2025-06-11 21:01:35 -0300

    Llamadas API productos

---
+++ src/pages/Producto.jsx
@@ -0,0 +1,64 @@
+import {useState,useEffect} from 'react'
+import axios from 'axios'
+
+export function Producto({}){
+    function crearProducto(nombre,stock,descripcion,precio){
+        const datos = {
+            nombre: nombre,
+            stock: stock,
+            descripcion:descripcion,
+            precio: precio
+        }
+        axios.post('http://localhost:8080/api/producto',{datos})
+        .then(function(response){
+            console.log(response)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+    function buscarProductos(){
+        const [productos, setProductos] = useState([])
+        axios.get(`http://localhost:8080/api/producto`,{})
+        .then((data)=>{
+            setProductos(data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+    function mostrarProducto({id}){
+        const [producto,setProducto] = useState([])
+        axios.get(`http://localhost:8080/api/producto/${id}`,{})
+         .then((data)=>{
+            setProducto(data)
+        })
+        .catch((error)=>{
+            console.log(error)
+        })
+    }
+    function actualizarProducto({id,nombre,stock,descripcion,precio}){
+        const datos ={
+            nombre: nombre,
+            stock: stock,
+            descripcion:descripcion,
+            precio: precio
+        }
+        axios.put(`http://localhost:8080/api/producto/${id}`,{datos})
+        .then(function(response){
+            console.log(response)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+    function borrarProducto ({id}){
+        axios.delete(`http://localhost:8080/api/producto/${id}`,{})
+        .then(function(response){
+            console.log(response)
+        })
+        .catch(function(error){
+            console.log(error)
+        })
+    }
+}
